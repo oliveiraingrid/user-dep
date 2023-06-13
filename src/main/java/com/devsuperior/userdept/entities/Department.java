fddad418ba8b1024c77a2d3a066d63c140925adf
Ingrid Oliveira <ingridoliveira@outlook.com>
2023-06-13 15:44:55 -0300

    criação do projeto

---
+++ src/main/java/com/devsuperior/userdept/entities/Department.java
@@ -0,0 +1,30 @@
+package com.devsuperior.userdept.entities;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tb_department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
